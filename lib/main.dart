ecdad73870d57840605d5e02bd5534bf3a9df437
Hà Huy Khôi <17520647@gm.uit.edu.vn>
2020-06-23 22:29:24 +0700

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterapp/Pages/User/LoginPage.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: LoginPage(),
+));
+
+
+
+
+
+
+
+
+
